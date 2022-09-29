16acfcd10e7dfe0806dae3cd57a4bfb44167af8f
TheoToni <32997269+TheoToni@users.noreply.github.com>
2022-09-29 21:50:35 +0200

    adding localstorage

--- src/components/SingleNote.jsx
+++ src/components/SingleNote.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import styles from "./SingleNote.module.css";
+import { useEffect } from "react";
+
+const singleNoteFromLocalStorage = JSON.parse(
+  localStorage.getItem("singleNote")
+);
 
 function SingleNote(props) {
+  const [inputData, setInputData] = React.useState(singleNoteFromLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem("singleNote", JSON.stringify(inputData));
+  }, [inputData]);
+
+  function inputHandler(event) {
+    setInputData((value) => {
+      return {
+        ...value,
+        text: event.target.value,
+      };
+    });
+    console.log(inputData);
+  }
+
   const styles = {
     backgroundColor: props.backgroundcolor,
     color: props.color,
@@ -10,6 +31,8 @@ function SingleNote(props) {
   return (
     <div>
       <textarea
+        onChange={inputHandler}
+        value={inputData.text}
         style={styles}
         className="singleNote"
         name="Notes"
